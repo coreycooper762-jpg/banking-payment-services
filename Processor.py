a5b54887740021f3777dfe533810ed099d003e04
coreycooper762-jpg <coreycooper762@gmail.com>
2026-03-03 19:30:58 +0530

    Update Processor.py

--- Processor.py
+++ Processor.py
@@ -1,1 +1,26 @@
-#change 1
+{
+  "fields": {
+    "project": {
+      "key": "BPS"
+    },
+    "summary": "Test issue created from Postman",
+    "description": {
+      "type": "doc",
+      "version": 1,
+      "content": [
+        {
+          "type": "paragraph",
+          "content": [
+            {
+              "type": "text",
+              "text": "This issue was created via Postman to test the JIRA REST API."
+            }
+          ]
+        }
+      ]
+    },
+    "issuetype": {
+      "name": "Task"
+    }
+  }
+}
